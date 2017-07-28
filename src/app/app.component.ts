2f416ddbbddc106e9a00d940b36581a1f1319fc5
Eric Tiedeken <eric.tiedeken@b507.us>
2017-07-28 12:32:31 -0500

    Updated the title variable to explain the init function and how variables should be declared and initialized as well as TypeScript. See comments.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,19 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
-    title = 'ToDo App';
+
+    // TypeScript helps check code before runtime. Here title is always a string so we initialize title to be a string
+    title: string;
 
     // First the component constructor is run. The constructor should be used to initialize component variables. No logic
     constructor() { }
 
     // Second the component init function is run. The init function is where the starting logic of the component should occur
-    ngOnInit() { }
+    ngOnInit() {
+        // here we give title a value
+        // `this` is the component
+        this.title = 'ToDo App';
+    }
 
     // The last thing a component does is get destroyed. This happens when a component is no longer valid either by removal or page transition
     // This is a good place to destroy component variables to help with memory
