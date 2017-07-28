d6e8f32c8906a07b29c28f8751df48f5af6b0fb7
Eric Tiedeken <eric.tiedeken@b507.us>
2017-07-28 12:31:31 -0500

    Added ngOnInit and ngOnDestroy to explain the life-cycle of a component.  See comments for more info.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
-  selector: 'app-component',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-component',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ToDo App';
+export class AppComponent implements OnInit, OnDestroy {
+    title = 'ToDo App';
+
+    // First the component constructor is run. The constructor should be used to initialize component variables. No logic
+    constructor() { }
+
+    // Second the component init function is run. The init function is where the starting logic of the component should occur
+    ngOnInit() { }
+
+    // The last thing a component does is get destroyed. This happens when a component is no longer valid either by removal or page transition
+    // This is a good place to destroy component variables to help with memory
+    ngOnDestroy() { }
 }
